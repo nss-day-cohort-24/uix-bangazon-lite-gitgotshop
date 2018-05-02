50d29caff3f7d50827bf8afda60e2e2dda8a28ca
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-05-02 13:02:11 -0500

    carousel started

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import '../App.css';
 import H1 from '../components/H1.js';
-import main1 from '../img/main_1.jpeg';
+import Carousel from '../components/Carousel.js';
 
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <img className="w-100" src={main1} alt="" />
-                </div>
+                <Carousel />
                 <H1 />
             </div>
         );
