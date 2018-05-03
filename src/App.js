40667f97c1f6f34a95bfa9d1565884aae9b0842a
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-03 11:23:10 -0500

    group work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav.js';
+import Generate from './Generate';
 
 class App extends Component {
   render() {
     return (
       <div>
        <Nav />
+       <Generate />
       </div>
     );
   }
