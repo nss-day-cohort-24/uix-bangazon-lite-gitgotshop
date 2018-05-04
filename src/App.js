e92e373afb45c3134f5a912880f4f8dfaedc20a9
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-04 10:34:20 -0500

    removed donetreadme. created AddUser and moved it out of App. re-created db.json to show new users

--- src/App.js
+++ src/App.js
@@ -1,60 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav.js';
-var faker= require('faker');
-// import Generate from './Generate';
-// import RandomData from './Customers.js';
+import AddUser from './AddUser';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.getStuff = this.getStuff.bind(this);
-}
-
-  getStuff() {
-    fetch('http://localhost:3000/profiles')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log("RESPONSE", responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-
-  blah(){
-    let data = {
-      "username": faker.name.firstName(),
-      "passsword": faker.internet.password() ,
-      "phone": faker.phone.phoneNumberFormat(),
-      "email": faker.internet.email(),
-      "address": faker.address.streetAddress(),
-      "paymentOption": faker.random.number(),
-      "paymentHistory": ""
-    }
-
-    console.log("BLAH");
-
-    fetch('http://localhost:3000/profiles', {
-      method: 'POST', // or 'PUT'
-      body: JSON.stringify(data), // data can be `string` or {object}!
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
-    }).then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
-  }
-
   render() {
-    this.getStuff()
     return (
       <div>
        <Nav />
-       {/* <Generate /> */}
-       {/* <RandomData /> */}
-       <button onClick={this.blah.bind(this)}>TEST</button>
+       <AddUser />
       </div>
     );
   }
