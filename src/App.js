d09ff776f94d748b10f44990949b7bef9be5f2ac
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-04 14:41:07 -0500

    capitalization on newProduct

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav.js';
 import Products from './Product_map';
 import AddUser from './AddUser';
 import faker from 'faker';
-import MakeProduct from './components/NewProduct'
+import MakeProduct from './components/newProduct'
 
 
 class App extends Component {
