a133cdc723915520eda57b47093357b1dd50b30d
Dylan B <giglett21@gmail.com>
2018-05-04 08:54:10 -0700

    add a button to generate new product data to the product object in db.json. Still need to hook with product form to input user

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     this.getStuff = this.getStuff.bind(this);
 }
 
+//////// USER FACTORY ////////
   getStuff() {
     fetch('http://localhost:3000/profiles')
       .then((response) => response.json())
@@ -47,6 +48,31 @@ class App extends Component {
     .then(response => console.log('Success:', response));
   }
 
+  //////// PRODUCT FACTORY /////////////////
+ 
+  makeProduct(){
+    let data = {
+      "name": "HERE IS A NEW PRODUCT",
+      "product": faker.internet.password() ,
+      "userId": faker.phone.phoneNumberFormat(),
+      "data": faker.internet.email(),
+      "price": faker.address.streetAddress(),
+      "disc": faker.random.number(),
+    }
+
+    console.log("Making Product");
+
+    fetch('http://localhost:3000/Products', {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+  }
+
   render() {
     this.getStuff()
     return (
@@ -55,6 +81,7 @@ class App extends Component {
        {/* <Generate /> */}
        {/* <RandomData /> */}
        <button onClick={this.blah.bind(this)}>TEST</button>
+       <button onClick={this.makeProduct.bind(this)}>Make New Product Randomly</button>
       </div>
     );
   }
