67458270d3d4f8637e869f4d4c1c9fbf4ab33e3f
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-04 14:19:58 -0500

    cleanup from last merge...removing erroneous imports and fixing user collection where it was overwritten to profiles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav.js';
-// import Generate from './Generate';
-import RandomData from './Customers.js';
 import Products from './Product_map';
 import AddUser from './AddUser';
 import faker from 'faker';
-import MakeProduct from './components/NewProduct.js'
+import MakeProduct from './components/NewProduct'
 
 
 class App extends Component {
