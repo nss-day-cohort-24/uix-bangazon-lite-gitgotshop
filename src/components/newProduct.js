86ccef238bf8740a5768a57ebac585b694b7faef
Dylan B <giglett21@gmail.com>
2018-05-04 09:58:33 -0700

    adde the input to the makeProduct function

--- src/components/newProduct.js
+++ src/components/newProduct.js
@@ -3,17 +3,26 @@ import faker from 'faker';
 
 class MakeProduct extends Component {
 
+////// PRODUCT FACTORY /////////////
     makeProduct(){
         let data = {
-          "name": "HERE IS A NEW PRODUCT",
-          "product": faker.internet.password() ,
-          "userId": faker.phone.phoneNumberFormat(),
-          "data": faker.internet.email(),
-          "price": faker.address.streetAddress(),
-          "disc": faker.random.number(),
+          "name": this.refs.cityName.value,
+          "product": "" ,
+          "sellerID": 0,
+          "price": this.refs.price.value,
+          "desc": this.refs.desc.value,
+          "data": this.refs.data.value
         }
     
-        console.log("Making Product");
+    
+    //   inputting(){
+    //     let data = {
+    //       "name": this.refs.cityName.value,
+    //       "price": this.refs.price.value,
+    //       "desc": this.refs.desc.value,
+    //       "sellerID": 0,
+    //       "data": "Nash.json"
+    //     }
     
         fetch('http://localhost:3000/Products', {
           method: 'POST', // or 'PUT'
@@ -25,12 +34,18 @@ class MakeProduct extends Component {
         .catch(error => console.error('Error:', error))
         .then(response => console.log('Success:', response));
       }
-    
+
+    /////////// RENDER TO DOM   //////////////////////
+
       render() {
         return (
-          <div>
-           <button onClick={this.makeProduct.bind(this)}>Make New Product Randomly</button>
-          </div>
+            <div>
+                <input ref="cityName" type="text" placeholder="City Name"/>
+                <input ref="price" type="text" placeholder="Price"/>
+                <input ref="desc" type="text" placeholder="Description"/>
+                <input ref="data" type="text" placeholder="Data Product"/>
+                <button onClick={this.makeProduct.bind(this)}>Make New Product Randomly</button>
+            </div>
         );
       }
     }
