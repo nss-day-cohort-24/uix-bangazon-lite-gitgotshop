f8dbbd5c5a7178728b31c55f028e195c533c19ed
Dylan B <giglett21@gmail.com>
2018-05-04 09:21:11 -0700

    created NewProduct.js file.

---
+++ src/components/newProduct.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/Nav.js';
+import AddUser from './AddUser';
+import faker from 'faker';
+
+class App extends Component {
+
+    render(){
+        return(
+            <div> 
+                <h1>Products</h1>
+            </div>
+        )
+    }
+}
