5e1023712e32b7c91c84df547ae72c549050bdfb
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-05-07 09:37:58 -0500

    app.js conflict fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav.js';
-<<<<<<< HEAD
-// import Generate from './Generate';
-=======
->>>>>>> 6d3b7868b37b179009580329304d594aa6767b8c
 import Products from './Product_map';
 import AddUser from './AddUser';
 import faker from 'faker';
