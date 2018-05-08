13bfc41f93d085fe9f6b66c2b62a28ad5dbb68f1
Dylan B <giglett21@gmail.com>
2018-05-08 12:53:24 -0700

    added more to the editbutton and changed template

--- src/components/EditButton.js
+++ src/components/EditButton.js
@@ -7,18 +7,18 @@ class EditButton extends Component {
     }
     
     // EDIT  
-editStuff = () => {
+editStuff = (id) => {
     let data = {
         "name": "Boston SUCKS",
         "picture": "./Cities/Boston.jpg",
         "product": "DU7DGCT82zHc9X9",
-        "userId": "511-715-3988",
+        "sellerID": "15",
         "data": "Laurianne67@gmail.com",
         "price": "$8,000",
         "disc": "Boston is Massachusetts’ capital. Founded in 1630, it’s one of the oldest cities in the U.S. "
     }
   
-    fetch('http://localhost:3000/products/2', {
+    fetch('http://localhost:3000/products/', {
       method: 'PUT', // or 'POST'
       body: JSON.stringify(data), // data can be `string` or {object}!
       headers: new Headers({
