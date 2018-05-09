08c8fde3895b22bdff73f5b750c774e8a8bebffc
Kelsey Pintens <kelseypintens@Kelseys-MacBook-Pro.local>
2018-05-09 12:10:52 -0500

    in progress

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,13 +6,37 @@ class Profile extends Component {
     render() {
         return (
             <div>
+
+                <div className="container">
+                    <div>
+                        <img className="round-circle" src={main1} alt="photo"/>
+                        <input id="Upload-photo" name="Upload photo" class="input-file" type="file"/>
+                        <label
+                            class="col-md-12 control-label text-center"
+                            for="Upload photo"
+                            accept=".jpg, .jpeg, .png">
+                        </label>
+                    </div>
+                        <legend class="text-center">SIGN UP</legend>
+                        <input id="Name (Full name)" name="Name (Full name)" type="text" placeholder="First Name" class="form-control input-md"/>
+                        <input id="Father" name="Father" type="text" placeholder="Last Name" class="form-control input-md"/>
+                        <input id="Phone number" name="Phone number" type="text" placeholder="Phone number " class="form-control input-md"/>
+                        <input id="Permanent Address" name="Permanent Address" type="text" placeholder="Permanent Address" class="form-control input-md "/>
+                        <input id="Permanent Address" name="Permanent Address" type="number" placeholder="zip-code" class="form-control input-md "/>
+                        <input id="Permanent Address" name="Permanent Address" type="text" placeholder="state"class="form-control input-md "/>
+                        <legend class="text">PAYMENT METHOD</legend>
+                        <span class="visible-xs-inline">CARD NUMBER</span>
+                    <div>
+                        
+                    </div>
+                </div>
+
 <div class = "container-fluid" >
-    <div class="row">
+    {/* <div class="row">
     <div class="col-xs-3 col-md-4 ">
         <div class="col-sm-4 col-md-4 col-lg-4">
             <div class="col-md-1">
                 <div class="col-md-2 hidden-xs">
-                    <img className="round-circle" src={main1} alt="photo"/>
                     <div class="form-group">
                         <div class="col-md-2">
                             <input id="Upload-photo" name="Upload photo" class="input-file" type="file"/>
@@ -20,12 +44,13 @@ class Profile extends Component {
                         <label
                             class="col-md-12 control-label text-center"
                             for="Upload photo"
-                            accept=".jpg, .jpeg, .png"></label>
+                            accept=".jpg, .jpeg, .png">
+                        </label>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div> */}
     <div class="col-xs-6 col-md-4 ">
         <div class="col-8 ">
             <legend class="text-center">SIGN UP</legend>
@@ -42,8 +67,7 @@ class Profile extends Component {
                     <div class="form-group">
                         <div class="col-md-8 p-2">
                             <div class="input-group">
-                                <input id="Father" name="Father" type="text" placeholder="Last Name"
-                                    class="form-control input-md"/>
+                                <input id="Father" name="Father" type="text" placeholder="Last Name" class="form-control input-md"/>
                             </div>
                         </div>
                     </div>
@@ -126,7 +150,7 @@ class Profile extends Component {
 <div class = "col-xs-3 col-md-4"/>
   
 </div>
-  </div>
+//   </div>
         );
     }
 }
