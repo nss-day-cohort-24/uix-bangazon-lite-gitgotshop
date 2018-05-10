d925f8a60a04e44edb53b4b6ff54863b098206f0
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-10 11:24:36 -0500

    remove ben

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -12,27 +12,7 @@
 //  */
 
 
-<<<<<<< HEAD
-function ProductCard(props) {
-        return (
-            <div className="d-flex flex-row">
-                <div className="my-4 justify-content-center">
-                    <img className="w-75" src={props.src} alt="" />
-                    <Link to={props.link}>
-                    <h3 className="my-2">Read More</h3></Link>
-                </div>
-                <div className="">
-                    <br />
-                    <h2>{props.name}</h2>
-                    <h3>{props.price}</h3>
-                    <p className="my-4">{props.disc}</p>
-                </div>
-            </div>
-        );
-    }
-=======
 // function ProductCard(props){
->>>>>>> master
 
 
 // if(this.props.printSingle === false){
